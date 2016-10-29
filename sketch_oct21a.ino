bcc759375215fda0a8d43ed0239a5ea1eac452f1
SheldonLee123 <1003807869@qq.com>
2016-10-29 20:10:42 +0800

    arduino

---
+++ sketch_oct21a.ino
@@ -0,0 +1,11 @@
+int ledPin=5;//设定控制LED的数字IO脚
+void setup(){
+  pinMode(ledPin,OUTPUT);//设定数字IO口的模式，OUTPUT为输出
+}
+void loop(){
+  digitalWrite(ledPin,HIGH);//设定PIN5脚为HIGH=5v左右
+  delay(2000);//设定延时时间，2000=2秒
+  digitalWrite(ledPin,LOW);//设定PIN5脚为LOW=0v
+  delay(2000);//设定延时时间，2000=2秒
+}
+
